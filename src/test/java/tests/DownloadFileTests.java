4cd97041ede8cb54e151975be84c8cc324e518c9
Dmitrii Kostenko <kdm.tag@gmail.com>
2021-06-26 15:09:30 +0300

    Added download file test

---
+++ src/test/java/tests/DownloadFileTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import utils.Files;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class DownloadFileTests {
+
+    @Test
+    public void selenideDownloadReadmeTest() throws IOException {
+
+        open("https://github.com/selenide/selenide/blob/master/README.md");
+        File downloadedFile = $("#raw-url").download();
+        String fileContent = Files.readTextFormFile(downloadedFile);
+
+        assertThat(fileContent, containsString("Selenide is based on and is compatible to Selenium WebDriver 2.0+ and 3.0+"));
+
+    }
+}
---
+++ src/test/java/utils/Files.java
@@ -0,0 +1,16 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Files {
+
+    public static String readTextFormFile(File file) throws IOException {
+
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+
+    }
+}
